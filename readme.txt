35f4ef5e1a3343114ecb7117e25b4c0751ac365c
Marian Kadanka <marian.kadanka@gmail.com>
2017-12-04 20:45:35 +0100

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== WP Change From Email ===
+Contributors: mariankadanka
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=marian.kadanka@gmail.com&item_name=Donation+for+Marian+Kadanka
+Tags: change, email, from, name
+Requires at least: 3.1
+Tested up to: 4.9
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Override the default From: 'WordPress <wordpress@mydomain.com>' name and email address.
+
+== Description ==
+
+Override the default From: 'WordPress <wordpress@mydomain.com>' name and email address.
+
+== Installation ==
+
+1. Go to Plugins > Add New > Search
+2. Type WP Change From Email in the search box and hit Enter
+3. Click on the button Install and then activate the plugin
+
+= Manual Installation =
+
+The manual installation method involves downloading our plugin and uploading it to your webserver via your favourite FTP application. The WordPress codex contains [instructions on how to do this here](http://codex.wordpress.org/Managing_Plugins#Manual_Plugin_Installation).
+
+== Screenshots ==
+
+1. WP Change From Email settings page
+
+== Changelog ==
+
+= 1.2 =
+* Plugin maintainer changed
+* Tweak: update admin settings page
+* Fix: replace deprecated function calls, other small compatibility fixes
+* Fix: code rewrite and indentation cleanup
---
+++ wp-from-email.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name: WP Change From Email
+Description: Override the default From: 'WordPress &lt;wordpress@mydomain.com&gt;' name and email address
+Version: 1.2
+Author: Marian Kadanka
+Author URI: https://github.com/marian-kadanka
+Text Domain: wp-from-email
+Domain Path: /languages
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+GitHub Plugin URI: https://github.com/marian-kadanka/wp-from-email
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( 'WPFromEmail' ) ):
+
+class WPFromEmail {
+
+	function __construct() {
+
+		if ( is_admin() ) {
+			add_filter( 'plugin_action_links', array( $this, 'action_links' ), 10, 4 );
+
+			add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+			add_action( 'admin_init', array( $this, 'settings_init' ) );
+		}
+
+		add_filter('wp_mail_from', array( $this, 'get_from_email' ) );
+		add_filter('wp_mail_from_name', array( $this, 'get_from_name' ) );
+
+		load_plugin_textdomain( 'wp-from-email', false, basename( dirname( __FILE__ ) ) . '/languages' );
+	}
+
+	function action_links( $links, $file ) {
+		if ( $file == plugin_basename( __FILE__ ) ) {
+			array_unshift( $links, '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=marian.kadanka@gmail.com&item_name=Donation+for+Marian+Kadanka" title="' . esc_attr__( 'Donate', 'wp-from-email' ) . '" target="_blank">' . esc_html__( 'Donate', 'wp-from-email' ) . '</a>' );
+
+			array_unshift( $links, '<a href="' . network_admin_url( 'options-general.php?page=wp_from_email' ) . '" title="' . esc_attr__( 'Settings', 'wp-from-email' ) . '">' . esc_html__( 'Settings', 'wp-from-email' ) . '</a>' );
+		}
+
+		return $links;
+	}
+
+	function add_admin_menu() {
+		add_options_page( 'WP From Email Settings', 'WP From Email', 'manage_options', 'wp_from_email', array( $this, 'options_page' ) );
+	}
+
+	function settings_init() {
+
+		register_setting( 'wp_from_email_settings', 'wp_from_email' );
+
+		add_settings_section(
+		'wp_from_email_settings_section', 
+		__( 'WP From Email Settings', 'wp-from-email' ), 
+		array( $this, 'settings_section_callback' ), 
+		'wp_from_email_settings'
+		);
+
+		add_settings_field( 
+		'email', 
+		__( 'From Email', 'wp-from-email' ), 
+		array( $this, 'email_render' ), 
+		'wp_from_email_settings', 
+		'wp_from_email_settings_section' 
+		);
+
+		add_settings_field( 
+		'name', 
+		__( 'From Name', 'wp-from-email' ), 
+		array( $this, 'name_render' ), 
+		'wp_from_email_settings', 
+		'wp_from_email_settings_section' 
+		);
+	}
+
+	function email_render() {
+		$options = get_option( 'wp_from_email' );
+		?>
+		<input type='text' name='wp_from_email[email]' value='<?php echo $options['email']; ?>'>
+		<?php
+	}
+
+	function name_render() {
+		$options = get_option( 'wp_from_email' );
+		?>
+		<input type='text' name='wp_from_email[name]' value='<?php echo $options['name']; ?>'>
+		<?php
+	}
+
+	function settings_section_callback() {
+		echo __( 'Override the default From: WordPress &lt;wordpress@mydomain.com&gt; name and email address', 'wp-from-email' );
+	}
+
+	function options_page() {
+		?>
+		<div class="wrap">
+		<form action='options.php' method='post'>
+
+		<?php
+		settings_fields( 'wp_from_email_settings' );
+		do_settings_sections( 'wp_from_email_settings' );
+		submit_button();
+		?>
+
+		</form>
+		</div>
+		<?php
+	}
+
+	function get_from_email( $email ) {
+		$options = get_option( 'wp_from_email' );
+		if ( $options && ! empty( $options['email'] ) ) {
+			return $options['email'];
+		}
+		else {
+			return $email;
+		}
+	}
+
+	function get_from_name( $name ) {
+		$options = get_option( 'wp_from_email' );
+		if ( $options && ! empty( $options['name'] ) ) {
+			return $options['name'];
+		}
+		else {
+			return $name;
+		}
+	}
+}
+endif;
+
+new WPFromEmail();
