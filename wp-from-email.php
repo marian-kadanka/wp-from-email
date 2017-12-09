cea5ce68ced92b49f1366c4d4c833b6e19d3c125
Marian Kadanka <marian.kadanka@gmail.com>
2017-12-09 12:24:32 +0100

    Copyright information added

--- wp-from-email.php
+++ wp-from-email.php
@@ -12,6 +12,26 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 GitHub Plugin URI: https://github.com/marian-kadanka/wp-from-email
 */
 
+/**
+ * WP Change From Email
+ * Copyright (C) 2008-2017 Skullbit.com. All rights reserved.
+ * Copyright (C) 2017 Marian Kadanka. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
